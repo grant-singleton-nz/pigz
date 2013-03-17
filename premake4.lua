34b854c918b1346144cc815d6367623f9fa43654
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2013-03-17 01:10:15 -0700

    move more options to a solution level

--- premake4.lua
+++ premake4.lua
@@ -2,16 +2,21 @@ solution "pigz"
    configurations { "Debug", "Release" }
    location "build"
 
+   flags { "Symbols", "StaticRuntime" }
+
    configuration "Debug"
        targetdir "dbg"
 
    configuration "Release"
        targetdir "rel"
+       flags { "Optimize" }
+       defines { "NDEBUG" }
+
+   configuration {"vs*"}
+      defines { "_WIN32", "WIN32", "WINDOWS", "_CRT_SECURE_NO_WARNINGS" }
 
 project "pigz"
-   targetname "pigz"
    kind "ConsoleApp"
-   location "build"
    language "C"
    files { "*.h", "*.c", "zopfli/*.h", "zopfli/*.c" }
    excludes { "zopfli/zopfli.c" }
@@ -19,34 +24,18 @@ project "pigz"
    links { "zlib" }
 
    configuration {"vs*"}
-      defines { "WIN32", "WINDOWS", "_CRT_SECURE_NO_WARNINGS", "NOTHREAD" }
+      defines { "NOTHREAD" }
       excludes { "yarn.c" }
       buildoptions {"/wd4996", "/wd4244", "/wd4305" }
       linkoptions {"/NODEFAULTLIB:\"msvcrt.lib\""}
 
    configuration "Debug"
        defines { "_DEBUG" }
-       flags { "Symbols", "StaticRuntime" }
-
-   configuration "Release"
-       defines { "NDEBUG" }
-       flags { "Optimize", "Symbols", "StaticRuntime" }
 
 project "zlib"
    kind "StaticLib"
-   location "build"
    language "C"
-   includedirs { "zlib" }
    files { "zlib/*.c", "zlib/*.h" }
-
    excludes { "zlib/gzclose.c", "zlib/gzread.c", "zlib/gzwrite.c", "zlib/gzlib.c"}
+   includedirs { "zlib" }
 
-   configuration {"vs*"}
-      defines { "_WIN32", "WINDOWS", "_CRT_SECURE_NO_WARNINGS" }
-
-   configuration "Debug"
-       flags { "Symbols", "StaticRuntime" }
-
-   configuration "Release"
-       defines { "NDEBUG" }
-       flags { "Optimize", "Symbols", "StaticRuntime" }
